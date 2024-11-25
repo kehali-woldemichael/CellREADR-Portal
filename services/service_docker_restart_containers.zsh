a7244050e9f9ee309163ae380f43cc3098c5b0e4
Kehali Woldemichael <kbw29@research-zjhuanglabweb-01.oit.duke.edu>
2024-11-25 10:05:46 -0500

    fix: service for restarting docker containers

--- services/service_docker_restart_containers.zsh
+++ services/service_docker_restart_containers.zsh
@@ -2,19 +2,18 @@
 
 
 function main() {
-  restart_container frontend
-  restart_container ensembl
+  restart_container test_frontend
+  #restart_container ensembl
 }
 
 function restart_container() {
   container_name="$1"
   # Step 1 check running containers 
-  status_frontend=$(docker inspect -f '{{.State.Status}}' "$container_name")
-  
+  status_container=$(docker inspect -f '{{.State.Status}}' "$container_name")
 
-  if [[ "$status_frontend" = "exited" ]]; then
+  if [[ "$status_container" = "exited" ]]; then
     echo "restarting container $container_name"
-    docker container start $container_name
+    docker container restart $container_name
     echo "\n"
   else
     echo "$container_name is not exited"
